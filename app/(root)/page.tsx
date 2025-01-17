06f7e954ca0fa263697e6f761427844b88284358
KGandhi90 <kevin.gandhi@somaiya.edu>
2025-01-18 00:19:10 +0530

    Added All Startups and Startup Card

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,23 @@
 import SearchForm from "@/components/SearchForm";
+import StartupCard from "@/components/StartupCard";
 
 export default async function Home({ searchParams }: {
   searchParams: Promise<{ query?: string }>
 }) {
   const query = (await searchParams).query;
+
+  const posts = [
+      { 
+        _createdAt : new Date(),
+        views: 55,
+        author: { _id: 1, name: 'Kevin' },
+        _id: 1,
+        description: "This is a description",
+        image: "https://www.shutterstock.com/image-vector/cartoon-floating-astronaut-vector-illustration-600nw-1673717851.jpg",
+        category: "Astronauts",
+        title: "Astronauts",
+    },
+  ];
   return (
     <>
       <section className="pink_container">
@@ -19,6 +33,21 @@ export default async function Home({ searchParams }: {
         <SearchForm query={query} />
       </section>
 
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : 'All Startups'}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupCardType, index: number) => (
+              <StartupCard key={post?._id} post={post}/>
+            ))
+          ) : (
+            <p className="no-results">No startups found</p>
+          )}
+        </ul>
+      </section>
     </>
   );
 }
